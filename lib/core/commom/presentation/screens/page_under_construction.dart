fb3f2aa8f7f7cea08fc67edb782b64f73661e6c1
FelipeHiroshi13 <felipehiroshi13@gmail.com>
2023-11-20 21:37:15 -0400

    feat: on_boarding and page under construction

---
+++ lib/core/commom/presentation/screens/page_under_construction.dart
@@ -0,0 +1,24 @@
+import 'package:education_app/core/res/media_res.dart';
+import 'package:flutter/material.dart';
+
+class PageUnderConstructions extends StatelessWidget {
+  const PageUnderConstructions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(MediaRes.onBoardingBackground),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
